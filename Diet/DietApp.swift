0883c606518cb470e7f6cdb8906033d09cf473a9
tmnabeel30 <tmnabeel30@gmail.com>
2024-07-29 08:42:45 +0530

    Initial Commit

---
+++ Diet/DietApp.swift
@@ -0,0 +1,17 @@
+//
+//  DietApp.swift
+//  Diet
+//
+//  Created by Nabeel on 29/07/24.
+//
+
+import SwiftUI
+
+@main
+struct DietApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
